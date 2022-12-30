4c4d2cc5fef73f3172edaa6f4ee10cfb6bc54788
Kim-777 <iloo3012@naver.com>
2022-12-30 10:14:58 +0900

    Update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>kim-skills</title>
+        <meta name="description" content="kimyechan portfolio index page" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:title" content="Kim yechan Portfolio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/among.jpeg" />
+        <meta property="og:description" content="김예찬의 포트폴리오" />
+        <link rel="icon" href="/circle-among.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,11 +17,11 @@ export default function Home() {
     <>
       <Head>
         <title>kimyechan</title>
-        <meta name="description" content="kimyechan portfolio index page" />
+        {/* <meta name="description" content="kimyechan portfolio index page" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:title" content="김예찬 포트폴리오" />
         <meta property="og:image" content="./among.jpeg"></meta>
-        <link rel="icon" href="/circle-among.png" />
+        <link rel="icon" href="/circle-among.png" /> */}
       </Head>
       <div style={{ whiteSpace: "pre-wrap", minHeight: "50vh" }}>
         <p className={cx({ texts: true })} style={{ color: selectedTextColor }}>
